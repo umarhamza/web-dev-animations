6fde47637a2eeb734918684faa1e2b94e019dece
Umar Hamza <umar@writemedia.co.uk>
2023-10-23 21:48:24 +0100

    working on portfolio

--- modern-portfolio/assets/js/main.js
+++ modern-portfolio/assets/js/main.js
@@ -3,32 +3,68 @@ gsap.registerPlugin(ScrollTrigger)
 const pinnedImageWrapper = document.getElementById("pinned-image-wrapper")
 const pinnedImage = document.getElementById("pinned-image")
 const pinnedContent = document.getElementById("pinned-content")
+const pinnedGradient = document.getElementById("pinned-gradient")
+const revealTypes = document.querySelectorAll('.reveal-type')
 
-gsap.set(pinnedImage, { height: window.innerHeight })
 
-const pinnedImageAnimation = gsap.to(pinnedImage, {
-    clipPath: 'inset(0% 0% 0%)',
-    scale: 1,
-    duration: 1,
-    ease: "Linear.easeNone",
-})
+function HeroImage() {
+    const triggerElement = pinnedImageWrapper
 
-ScrollTrigger.create({
-    trigger: pinnedImageWrapper,
-    start() {
-        const startPin = 0
-        return 'top +=' + startPin
-    },
-    end() {
-        const endPin = pinnedContent.offsetHeight;
-        return "bottom " + endPin;
-    },
-    animation: pinnedImageAnimation,
-    scrub: 1,
-    pin: true,
-    pinSpacing: false,
-    markers: true
-})
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: triggerElement,
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 1,
+        }
+    })
+
+    tl.to(pinnedImage, {
+        clipPath: 'inset(0% 0% 0%)',
+        scale: 1,
+        duration: 1,
+        ease: "Linear.easeNone",
+    })
+
+    tl.fromTo(pinnedGradient, {
+        opacity: 0,
+    }, {
+        opacity: 1,
+        duration: 1,
+        ease: "Linear.easeNone",
+    })
+}
+
+HeroImage()
+
+function RevealTypes() {
+    revealTypes.forEach((heading) => {
+        if (!heading) return
+
+        const text = new SplitType(heading, {
+            types: 'chars'
+        })
+
+        gsap.fromTo(text.chars, {
+            opacity: 0.2
+
+        }, {
+            opacity: 1,
+            duration: 0.1,
+            stagger: 0.02,
+            scrollTrigger: {
+                trigger: heading,
+                start: 'top 80%',
+                end: 'bottom 90%',
+                scrub: true,
+                markers: false,
+                toggleActions: 'play play reverse reverse'
+            }
+        })
+    })
+}
+
+RevealTypes()
 
 const lenis = new Lenis()
 
